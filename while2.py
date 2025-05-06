b8f4fd768984d526eed1ff3c0063d03f398f7de8
BaZa06 <bazeledon@uamv.edu.ni>
2025-05-06 07:52:17 -0600

    Se realizo un archivo a la salida

--- while2.py
+++ while2.py
@@ -1,7 +1,7 @@
-"""Leer una palabra diferente a fin y convertirla a mayuscula"""
+"""Leer una palabra e indicar su número de letras"""
 
 palabra = input("Escribe una palabra: ")
 while palabra.lower() != "fin":
-    print(palabra.upper())
+    print(f"{palabra.upper()} tiene {len(palabra)} letras")
     palabra = input("Escribe una palabra: ")
 else: print("Adiós...")
